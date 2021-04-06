9792d1e1f6aba88acc5dba67078c03929f7a8e5e
maxim.sherstoboyev@uco.kz <maxim.sherstoboyev@uco.kz>
2021-04-06 19:14:14 +0600

    add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var cors = require('cors');
 
 var app = express();
 
@@ -11,8 +12,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/**', indexRouter);
+app.use('/**', cors(), indexRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,22 @@ const amoCrmFetch = require('../api/amoCrm/index');
 
 router.get('/*', function (req, res, next) {
     const amoApiCrmUrl = extractApiFromUrl(req.baseUrl);
+    const token = props.get('token');
+
     amoCrmFetch.get(amoApiCrmUrl, {
         headers: {
-            Authorization: 'Bearer ' + props.get('token')
-        }
+            Authorization: 'Bearer ' + token
+        },
+        params: req.query
+        // params: {
+        //     query: 40246067,
+        //     with: 'contacts'
+        // }
     }).then(response => {
         res.send(response.data);
     }).catch(reason => {
         console.log(reason);
+        res.send();
     });
 });
 
